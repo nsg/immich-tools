d6c2e4f958f26a60dddb36a4f022ec80c4a4e442
Stefan Berggren <nsg@nsg.cc>
2023-01-17 00:53:28 +0100

    Log deletions in a new table, change checksum API
    Create a TRIGGER on delete operations on the asset table, store the
    checksums of the deleted images. Expose it with an API endpoint.
    
    This commit also changes the checksum API, it now returns a list.

--- services/api/src/database.py
+++ services/api/src/database.py
@@ -1,4 +1,5 @@
 import os
+import base64
 
 import psycopg2
 import psycopg2.extras
@@ -20,13 +21,68 @@ class ImmichDatabase:
             password=password,
             port=port)
 
-        self.cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
     def list_users(self):
-        self.cursor.execute("SELECT * FROM users")
-        return self.cursor.fetchall()
+        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("SELECT * FROM users")
+        r = []
+        for user in cursor:
+            r.append(user['id'])
+        cursor.close()
+        return r
 
     def get_asset_checksum(self, checksum):
         checksum = f"\\x{checksum}"
-        self.cursor.execute("SELECT id FROM assets WHERE checksum = %s", (checksum,))
-        return self.cursor.fetchall()
+        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("SELECT id FROM assets WHERE checksum = %s", (checksum,))
+        r = []
+        for asset in cursor:
+            r.append(asset['id'])
+        cursor.close()
+        return { "assets": r, "count": len(r) }
+
+
+    def list_deleted_assets_last_n_minutes(self, minutes):
+        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("SELECT checksum FROM assets_delete_audits WHERE changed_on > (NOW() - interval '%s minutes')", (minutes,))
+        r = []
+        for deleted in cursor:
+            r.append(bytes(deleted['checksum']).hex())
+        cursor.close()
+        return r
+
+    def provision_delete_trigger(self):
+        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS assets_delete_audits (
+                id INT GENERATED ALWAYS AS IDENTITY,
+                asset_id UUID NOT NULL,
+                checksum BYTEA,
+                changed_on TIMESTAMP(6) NOT NULL
+            );
+        """)
+
+        cursor.execute("""
+            CREATE OR REPLACE FUNCTION log_assets_delete_audits()
+                RETURNS TRIGGER
+                LANGUAGE PLPGSQL
+                AS
+            $$
+            BEGIN
+                INSERT INTO assets_delete_audits(asset_id,checksum,changed_on)
+                VALUES(OLD.id, OLD.checksum, NOW());
+                RETURN OLD;
+            END;
+            $$
+        """)
+
+        cursor.execute("""
+            CREATE OR REPLACE TRIGGER trigger_assets_delete_audits
+            BEFORE DELETE ON assets
+            FOR EACH ROW
+            EXECUTE PROCEDURE log_assets_delete_audits()
+        """)
+
+        cursor.close()
+        self.conn.commit()
--- services/api/src/main.py
+++ services/api/src/main.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
@@ -14,6 +16,7 @@ app = FastAPI(
 )
 
 db = ImmichDatabase()
+db.provision_delete_trigger()
 
 @app.get("/", include_in_schema=False)
 def redirect_to_docs():
@@ -22,13 +25,14 @@ def redirect_to_docs():
 
 @app.get("/users")
 def get_users():
-    users = db.list_users()
-    return { x['id'] for x in users }
+    return db.list_users()
 
 @app.get("/asset/checksum/{checksum}")
 def get_asset_checksum(checksum: str):
     if len(checksum) % 2 == 0:
-        assets = db.get_asset_checksum(checksum)
-        if assets:
-            return { "assets": x for x in assets }
-    return { "assets": {} }
+        return db.get_asset_checksum(checksum)
+    return { "assets": [], "count": 0 }
+
+@app.get("/asset/deleted")
+def get_asset_deleted():
+    return db.list_deleted_assets_last_n_minutes(5)
