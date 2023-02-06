3f78c255edad54dd5240e23be1e7babdfd3893e6
Stefan Berggren <nsg@nsg.cc>
2023-02-06 09:35:11 +0100

    Open a new db connection for each query
    This is not an performance critical application and this was a simple
    workaround for my cache problems. This is also technically simpler and
    more reliable with is a nice little trade off.

--- services/api/src/database.py
+++ services/api/src/database.py
@@ -6,34 +6,50 @@ import psycopg2.extras
 
 
 class ImmichDatabase:
-    def __init__(self) -> None:
-        database = os.getenv("DB_DATABASE_NAME", "immich")
-        host = os.getenv("DB_HOSTNAME", "127.0.0.1")
-        username = os.getenv("DB_USERNAME", "postgres")
-        password = os.getenv("DB_PASSWORD", "postgres")
-        port = os.getenv("DB_PORT", 5432)
 
-        self.conn = psycopg2.connect(
-            database=database, host=host, user=username, password=password, port=port
+    database: str
+    host: str
+    username: str
+    password: str
+    port: int
+
+    def __init__(self) -> None:
+        self.database = os.getenv("DB_DATABASE_NAME", "immich")
+        self.host = os.getenv("DB_HOSTNAME", "127.0.0.1")
+        self.username = os.getenv("DB_USERNAME", "postgres")
+        self.password = os.getenv("DB_PASSWORD", "postgres")
+        self.port = int(os.getenv("DB_PORT", 5432))
+
+    def query(self, sql: str, args: tuple = ()):
+        r = []
+        conn = psycopg2.connect(
+            database=self.database,
+            host=self.host,
+            user=self.username,
+            password=self.password,
+            port=self.port,
         )
-        self.conn.autocommit = True
+        cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute(sql, args)
+        if cursor.rowcount > 0:
+            for row in cursor:
+                r.append(dict(row))
+        cursor.close()
+        conn.close()
+        return r
 
     def list_users(self):
-        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-        cursor.execute("SELECT * FROM users")
         r = []
-        for user in cursor:
+        for user in self.query("SELECT * FROM users"):
             r.append(user["id"])
-        cursor.close()
         return r
 
     def get_externalfile_by_checksum(self, checksum):
-        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-        cursor.execute(
+        q = self.query(
             "SELECT * FROM hasher_scanned_files WHERE checksum = %s", (checksum,)
         )
         r = []
-        for asset in cursor:
+        for asset in q:
             r.append(
                 {
                     "id": asset["id"],
@@ -42,32 +58,28 @@ class ImmichDatabase:
                     "changed": asset["changed_on"],
                 }
             )
-        cursor.close()
         return {"assets": r, "count": len(r)}
 
     def get_asset_checksum(self, checksum, user_id):
         checksum = f"\\x{checksum}"
-        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
         if user_id:
-            cursor.execute(
+            q = self.query(
                 'SELECT id, "userId" FROM assets WHERE checksum = %s AND "userId" = %s',
                 (checksum, user_id),
             )
         else:
-            cursor.execute(
+            q = self.query(
                 'SELECT id, "userId" FROM assets WHERE checksum = %s', (checksum,)
             )
 
         r = []
-        for asset in cursor:
+        for asset in q:
             r.append({"asset_id": asset["id"], "user_id": asset["userId"]})
-        cursor.close()
         return {"assets": r, "count": len(r)}
 
     def list_last_deleted_assets(self):
-        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-        cursor.execute(
+        q = self.query(
             """
             SELECT asset_id, user_id, checksum, changed_on
                 FROM assets_delete_audits
@@ -75,7 +87,7 @@ class ImmichDatabase:
             """
         )
         r = []
-        for deleted in cursor:
+        for deleted in q:
             row = {
                 "id": deleted["asset_id"],
                 "user_id": deleted["user_id"],
@@ -83,13 +95,10 @@ class ImmichDatabase:
                 "changed": deleted["changed_on"],
             }
             r.append(row)
-        cursor.close()
         return r
 
     def provision_delete_trigger(self):
-        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-
-        cursor.execute(
+        self.query(
             """
             CREATE TABLE IF NOT EXISTS assets_delete_audits (
                 id INT GENERATED ALWAYS AS IDENTITY,
@@ -101,7 +110,7 @@ class ImmichDatabase:
         """
         )
 
-        cursor.execute(
+        self.query(
             """
             CREATE OR REPLACE FUNCTION log_assets_delete_audits()
                 RETURNS TRIGGER
@@ -117,7 +126,7 @@ class ImmichDatabase:
         """
         )
 
-        cursor.execute(
+        self.query(
             """
             CREATE OR REPLACE TRIGGER trigger_assets_delete_audits
             BEFORE DELETE ON assets
@@ -125,6 +134,3 @@ class ImmichDatabase:
             EXECUTE PROCEDURE log_assets_delete_audits()
         """
         )
-
-        cursor.close()
-        self.conn.commit()
